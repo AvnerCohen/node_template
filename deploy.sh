aa1162e7740c1f8b8bd34d3fb1db53269b48d901
Avner Cohen <israbirding@gmail.com>
2013-11-04 15:39:54 +0200

    Updated Deploy Script

--- deploy.sh
+++ deploy.sh
@@ -18,34 +18,29 @@ curl http://localhost:${PORT}/ping
 
 deploy ()
 {
-        check_app
-         if [ $? -ne "0" ]
-          then
-             echo "${APP} is down , starting..."
-             cd ${CUR_DIR}
-             npm start
-         fi
-        
-        echo "Deploying ${APP}....${PWD}..${whoami}"
-        PREV_WORKING_REVISION="$(cat ${CUR_DIR}/run/.RUNNING_REVISION || echo "")"
-        echo "Prev Revision ${PREV_WORKING_REVISION}"
-        NEXT_WORKING_REVISION=`git rev-parse HEAD`
-        echo "Next Revision ${NEXT_WORKING_REVISION}"
-        npm install
-        npm run-script deploy
-        echo `git rev-parse HEAD` > ${CUR_DIR}/run/.RUNNING_REVISION
+    check_app
+    if [ $? -ne "0" ]
+    then
+       echo "${APP} is down , starting..."
+       cd ${CUR_DIR}
+       npm start
+    fi
 
-        check_app
-          if [ $? -ne "0" ]
-             then
-              echo "Deploy failed for ${APP}, rolling back"
-              cd ${CUR_DIR}
-              git checkout ${PREV_WORKING_REVISION}
-              npm install
-              npm start
-              exit 1
-          fi
-
+    echo "Deploying ${APP}....${PWD}..${whoami}"
+    PREV_WORKING_REVISION="$(cat ${CUR_DIR}/run/.RUNNING_REVISION || echo "")"
+    echo "Prev Revision ${PREV_WORKING_REVISION}"
+    NEXT_WORKING_REVISION=`git rev-parse HEAD`
+    echo "Next Revision ${NEXT_WORKING_REVISION}"
+    npm install
+    npm run-script deploy
+    rc=$?
+    if [ $rc -eq "0" ]
+    then
+      echo `git rev-parse HEAD` > ${CUR_DIR}/run/.RUNNING_REVISION
+      check_app
+    else
+      exit $rc
+    fi
 }
 
 
